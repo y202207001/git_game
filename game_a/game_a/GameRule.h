f4b88c165fe00a2d9d83c9288a8b9cc7200da833
geonwoomoon <aromajung1@naver.com>
2025-06-14 12:31:39 +0900

    [class화 하지말고 main.cpp에서 구현 후 추후 class로 세분화]

---
+++ game_a/game_a/GameRule.h
@@ -0,0 +1,6 @@
+#pragma once
+
+class GameRule {
+public:
+    int determineWinner(int choice1, int choice2) const;
+};
---
+++ game_a/game_a/Player.cpp
@@ -0,0 +1,6 @@
+#include "Player.h"
+
+Player::Player(std::string name)
+    : name(name), score(0), choice(0)
+{
+}
